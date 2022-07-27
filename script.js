3a7b395c667543cd0d7c644c6afe5e09692db6ea
Luiz Porto <piilamster@gmail.com>
2022-07-27 16:55:09 -0300

    scroll suave e animação

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ initTabNav();
 
 function initAccordion() {
   const accordionList = document.querySelectorAll(".js-accordion dt");
-  const activeClass = 'ativo';
-  
+  const activeClass = "ativo";
+
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
@@ -40,3 +40,49 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksIntenos = document.querySelectorAll('.js-menu a[href^="#]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    //forma alternativa
+    //const topo = section.offsetTop;
+    // window.scrollTo({
+    //  top: topo,
+    //  behavior: 'smooth',
+    // });
+  }
+
+  linksIntenos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.5;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
